65cdf252c7d7d0afba80afb661f65926a63c0007
SeokWon <tjrdnjs1534@naver.com>
2022-08-26 21:17:48 +0900

    Add filter function

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,9 +7,23 @@ import "./Expenses.css";
 const Expenses = (props) => {
   const [filteredYear, setFilterYear] = useState("2022");
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
   const filterChangeHandler = (selectedYear) => {
     setFilterYear(selectedYear);
   };
+  let expensesContent = <p>No Expenses found. </p>;
+  if(filteredExpenses.length > 0){
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
 
   return (
     <Card className="expenses">
@@ -17,15 +31,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (      
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-      ;
+      {expensesContent}
     </Card>
   );
 };
